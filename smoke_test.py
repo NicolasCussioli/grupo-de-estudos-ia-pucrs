c39fc534f325e9f82bf22fa398bfa3aa7453cd4a
Nicolas Cussioli <n.cussioli@edu.pucrs.br>
2026-09-14 20:07:58 -0300

    Add smoke_test.py for Python study setup

---
+++ smoke_test.py
@@ -0,0 +1,8 @@
+import platform
+import sys
+
+print('=' * 55)
+print('Ambiente do Grupo de Estudos IA - Configurado com Sucesso!')
+print(f'Versao do Python: {sys.version.split()[0]}')
+print(f'Sistema Operacional: {platform.system()} {platform.release()}')
+print('=' * 55)
